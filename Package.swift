5630515f0df9b710cbbc34657d9630e3d5932317
David Okun <david@okun.io>
2019-08-16 21:48:02 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,16 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "kitura-on-heroku",
     dependencies: [
-       .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
-       .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0)
+      .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.7.1"),
+      .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.9.0")
+    ],
+    targets: [
+        .target(
+            name: "kitura-on-heroku",
+            dependencies: ["Kitura", "HeliumLogger"])
     ])
